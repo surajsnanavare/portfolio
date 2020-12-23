3a10554a551ba67f8668f6c3abb554d0a88e407f
Sushant Patil <sushant.patil@springct.com>
2020-12-23 20:20:23 +0530

    initial commit

---
+++ build/precache-manifest.3fab86bcf7c17f5e430cdc33d79e8daa.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8367424dc9e314e0062844c9a430c623",
+    "url": "/index.html"
+  },
+  {
+    "revision": "7c28bf955b3b47cb6c32",
+    "url": "/static/css/2.22a7d4ef.chunk.css"
+  },
+  {
+    "revision": "95d1c2972c6f21aa1cf8",
+    "url": "/static/css/main.162d6878.chunk.css"
+  },
+  {
+    "revision": "7c28bf955b3b47cb6c32",
+    "url": "/static/js/2.6bd549d3.chunk.js"
+  },
+  {
+    "revision": "95d1c2972c6f21aa1cf8",
+    "url": "/static/js/main.93c716df.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "43f62b103c33f53ea8becff3f1725ac6",
+    "url": "/static/media/Vmarine.43f62b10.jpg"
+  },
+  {
+    "revision": "851ac5cd41c1272715245e19eb765cce",
+    "url": "/static/media/aguaDeLuz.851ac5cd.png"
+  },
+  {
+    "revision": "32baf6b217a73a233632d2d5241f5c3b",
+    "url": "/static/media/myImage.32baf6b2.jpeg"
+  },
+  {
+    "revision": "ac82d97d170d742395675707f640e98d",
+    "url": "/static/media/todolist.ac82d97d.png"
+  }
+]);
